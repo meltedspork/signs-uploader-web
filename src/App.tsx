56dcd16208b47dfc179e0c4d9e8d29d8e568f4c0
Jason Salerno <jason@meltedspork.com>
2021-03-27 11:33:08 -0500

    use correct react router library import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
 
 import NavigationLinks from './components/NavigationLinks';
 import Home from './views/Home';
